1748eb7d4602a1bece5c9a60c4aa39fe2bf509ae
Marcelo Ferraz de Oliveira <mfogoiania@gmail.com>
2025-10-27 04:13:12 -0300

    aula 34

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatAnchor, MatButtonModule } from "@angular/material/button";
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    MatTableModule,
+    MatAnchor
+],
   templateUrl: './consulta.html',
   styleUrl: './consulta.scss'
 })
